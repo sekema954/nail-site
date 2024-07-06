0be097d429d63e3e571e0ef9621cbb4aa716c0bf
Samuel Ekema <ekema@DESKTOP-2VC7FTM>
2024-07-06 01:04:15 -0400

    initial commit

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ menubarClicked();
 function menubarClicked(){
     const menubar = document.querySelector('.menu-bar');
     const dropdown = document.querySelector('.dropdown-menu');
+    const dropdownLinks = document.querySelector('.dropdown-links');
     menubar.addEventListener('click', ()=>{
         menubar.classList.toggle('change');
         dropdown.classList.toggle('toggleDropdown');
+        dropdownLinks.classList.toggle('dropdown-links-active');
 
     })
 };
